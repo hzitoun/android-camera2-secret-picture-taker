91b97751bc2211853405505da00d14b24b2a8c40
XieEDeHeiShou <qq2325690622@gmail.com>
2018-03-28 23:19:00 +0800

    Reformat

--- app/src/main/java/com/hzitoun/camera2SecretPictureTaker/services/PictureCapturingServiceImpl.java
+++ app/src/main/java/com/hzitoun/camera2SecretPictureTaker/services/PictureCapturingServiceImpl.java
@@ -37,7 +37,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.TreeMap;
-import java.util.UUID;
 
 
 /**
@@ -52,19 +51,17 @@ public class PictureCapturingServiceImpl extends APictureCapturingService {
 
     private static final String TAG = PictureCapturingServiceImpl.class.getSimpleName();
 
-    private CameraDevice cameraDevice;
-    private ImageReader imageReader;
     /***
      * camera ids queue.
      */
     private Queue<String> cameraIds;
-    
+
     private String currentCameraId;
-    private boolean cameraClosed;
     /**
      * stores a sorted map of (pictureUrlOnDisk, PictureData).
      */
     private TreeMap<String, byte[]> picturesTaken;
+
     private PictureCapturingListener capturingListener;
 
     /***
@@ -96,8 +93,7 @@ public class PictureCapturingServiceImpl extends APictureCapturingService {
             final String[] cameraIds = manager.getCameraIdList();
             if (cameraIds.length > 0) {
                 this.cameraIds.addAll(Arrays.asList(cameraIds));
-                this.currentCameraId = this.cameraIds.poll();
-                openCamera();
+                openCamera(this.cameraIds.poll());
             } else {
                 //No camera detected!
                 capturingListener.onDoneCapturingAllPhotos(picturesTaken);
@@ -107,7 +103,8 @@ public class PictureCapturingServiceImpl extends APictureCapturingService {
         }
     }
 
-    private void openCamera() {
+    private void openCamera(@NonNull String currentCameraId) {
+        this.currentCameraId = currentCameraId;
         Log.d(TAG, "opening camera " + currentCameraId);
         try {
             if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
@@ -115,90 +112,61 @@ public class PictureCapturingServiceImpl extends APictureCapturingService {
                     && ActivityCompat.checkSelfPermission(context,
                     Manifest.permission.WRITE_EXTERNAL_STORAGE)
                     == PackageManager.PERMISSION_GRANTED) {
-                manager.openCamera(currentCameraId, stateCallback, null);
+                manager.openCamera(currentCameraId, cameraDeviceStateCallback(),/*work handler*/ null);
             }
         } catch (final CameraAccessException e) {
             Log.e(TAG, " exception occurred while opening camera " + currentCameraId, e);
         }
     }
 
-    private final CameraCaptureSession.CaptureCallback captureListener = new CameraCaptureSession.CaptureCallback() {
-        @Override
-        public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request,
-                                       @NonNull TotalCaptureResult result) {
-            super.onCaptureCompleted(session, request, result);
-            if (picturesTaken.lastEntry() != null) {
-                capturingListener.onCaptureDone(picturesTaken.lastEntry().getKey(), picturesTaken.lastEntry().getValue());
-                Log.i(TAG, "done taking picture from camera " + cameraDevice.getId());
+    private CameraDevice.StateCallback cameraDeviceStateCallback() {
+        return new CameraDevice.StateCallback() {
+
+            @Override
+            public void onOpened(@NonNull CameraDevice camera) {
+                Log.d(TAG, "camera " + camera.getId() + " opened");
+                Log.i(TAG, "Taking picture from camera " + camera.getId());
+                //Take the picture after some delay. It may resolve getting a black dark photos.
+                new Handler().post(() -> {
+                    try {
+                        takePicture(camera);
+                    } catch (final CameraAccessException e) {
+                        Log.e(TAG, " exception occurred while taking picture from " + currentCameraId, e);
+                    }
+                });// no more delay
             }
-            closeCamera();
-        }
-    };
 
+            @Override
+            public void onDisconnected(@NonNull CameraDevice camera) {
+                // this device is no longer available, CameraAccessException
+                Log.d(TAG, " camera " + camera + " disconnected");
+            }
 
-    private final ImageReader.OnImageAvailableListener onImageAvailableListener = (ImageReader imReader) -> {
-        final Image image = imReader.acquireLatestImage();
-        final ByteBuffer buffer = image.getPlanes()[0].getBuffer();
-        final byte[] bytes = new byte[buffer.capacity()];
-        buffer.get(bytes);
-        saveImageToDisk(bytes);
-        image.close();
-    };
-
-    private final CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
-        @Override
-        public void onOpened(@NonNull CameraDevice camera) {
-            cameraClosed = false;
-            Log.d(TAG, "camera " + camera.getId() + " opened");
-            cameraDevice = camera;
-            Log.i(TAG, "Taking picture from camera " + camera.getId());
-            //Take the picture after some delay. It may resolve getting a black dark photos.
-            new Handler().postDelayed(() -> {
-                try {
-                    takePicture();
-                } catch (final CameraAccessException e) {
-                    Log.e(TAG, " exception occurred while taking picture from " + currentCameraId, e);
+            @Override
+            public void onClosed(@NonNull CameraDevice camera) {
+                // this device is no longer available, IllegalStateException
+                Log.d(TAG, "camera " + camera + " closed");
+                //once the current camera has been closed, start taking another picture
+                if (!cameraIds.isEmpty()) {
+                    takeAnotherPicture();
+                } else {
+                    capturingListener.onDoneCapturingAllPhotos(picturesTaken);
                 }
-            }, 500);
-        }
-
-        @Override
-        public void onDisconnected(@NonNull CameraDevice camera) {
-            Log.d(TAG, " camera " + camera.getId() + " disconnected");
-            if (cameraDevice != null && !cameraClosed) {
-                cameraClosed = true;
-                cameraDevice.close();
             }
-        }
 
-        @Override
-        public void onClosed(@NonNull CameraDevice camera) {
-            cameraClosed = true;
-            Log.d(TAG, "camera " + camera.getId() + " closed");
-            //once the current camera has been closed, start taking another picture
-            if (!cameraIds.isEmpty()) {
-                takeAnotherPicture();
-            } else {
-                capturingListener.onDoneCapturingAllPhotos(picturesTaken);
+            private void takeAnotherPicture() {
+                openCamera(cameraIds.poll());
             }
-        }
 
-
-        @Override
-        public void onError(@NonNull CameraDevice camera, int error) {
-            Log.e(TAG, "camera in error, int code " + error);
-            if (cameraDevice != null && !cameraClosed) {
-                cameraDevice.close();
+            @Override
+            public void onError(@NonNull CameraDevice camera, int error) {
+                Log.e(TAG, "camera in error, int code " + error);
+                camera.close();
             }
-        }
-    };
+        };
+    }
 
-
-    private void takePicture() throws CameraAccessException {
-        if (null == cameraDevice) {
-            Log.e(TAG, "cameraDevice is null");
-            return;
-        }
+    private void takePicture(@NonNull CameraDevice cameraDevice) throws CameraAccessException {
         final CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraDevice.getId());
         Size[] jpegSizes = null;
         StreamConfigurationMap streamConfigurationMap = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
@@ -211,56 +179,85 @@ public class PictureCapturingServiceImpl extends APictureCapturingService {
         final ImageReader reader = ImageReader.newInstance(width, height, ImageFormat.JPEG, 1);
         final List<Surface> outputSurfaces = new ArrayList<>();
         outputSurfaces.add(reader.getSurface());
-        final CaptureRequest.Builder captureBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
-        captureBuilder.addTarget(reader.getSurface());
-        captureBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
-        captureBuilder.set(CaptureRequest.JPEG_ORIENTATION, getOrientation());
-        reader.setOnImageAvailableListener(onImageAvailableListener, null);
-        cameraDevice.createCaptureSession(outputSurfaces, new CameraCaptureSession.StateCallback() {
-                    @Override
-                    public void onConfigured(@NonNull CameraCaptureSession session) {
-                        try {
-                            session.capture(captureBuilder.build(), captureListener, null);
-                        } catch (final CameraAccessException e) {
-                            Log.e(TAG, " exception occurred while accessing " + currentCameraId, e);
-                        }
-                    }
 
-                    @Override
-                    public void onConfigureFailed(@NonNull CameraCaptureSession session) {
-                    }
+        // The key to achieve non-preview capture:
+        // 1. Output into the ImageReader's surface.
+        // 2. target the ImageReader's surface, so that you can get Image form listener.
+        cameraDevice.createCaptureSession(outputSurfaces, cameraCaptureSessionStateCallback(cameraDevice, reader), null);
+
+        // So if you want to switch between preview and non-preview,
+        // just replace the target and output with ImageReader's SurfaceView or anther available SurfaceView
+    }
+
+    @NonNull
+    private CameraCaptureSession.StateCallback cameraCaptureSessionStateCallback(
+            @NonNull final CameraDevice cameraDevice, @NonNull final ImageReader reader) {
+        return new CameraCaptureSession.StateCallback() {
+            @Override
+            public void onConfigured(@NonNull CameraCaptureSession session) {
+                try {
+                    session.capture(request(), captureCallback(), null);
+                } catch (final CameraAccessException e) {
+                    Log.e(TAG, " exception occurred while accessing " + currentCameraId, e);
                 }
-                , null);
+            }
+
+            private CaptureRequest request() throws CameraAccessException {
+                CaptureRequest.Builder builder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+                builder.addTarget(reader.getSurface());
+                builder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+                builder.set(CaptureRequest.JPEG_ORIENTATION, getOrientation());
+                reader.setOnImageAvailableListener(
+                        (ImageReader imReader) -> {
+                            final Image image = imReader.acquireLatestImage();
+                            final ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+                            final byte[] bytes = new byte[buffer.capacity()];
+                            buffer.get(bytes);
+                            saveImageToDisk(bytes, cameraDevice.getId());
+                            image.close();
+                        },
+                        /*work handler*/ null);//OPTIMIZE: work in a separate HandlerThread
+                return builder.build();
+            }
+
+            private void saveImageToDisk(@NonNull final byte[] bytes, @NonNull String cameraId) {
+                final File file = new File(Environment.getExternalStorageDirectory() + "/" + cameraId + "_pic.jpg");
+                try (final OutputStream output = new FileOutputStream(file)) {
+                    output.write(bytes);
+                    picturesTaken.put(file.getPath(), bytes);
+                } catch (final IOException e) {
+                    Log.e(TAG, "Exception occurred while saving picture to external storage ", e);
+                }
+            }
+
+            private CameraCaptureSession.CaptureCallback captureCallback() {
+                return new CameraCaptureSession.CaptureCallback() {
+                    @Override
+                    public void onCaptureCompleted(@NonNull CameraCaptureSession session,
+                                                   @NonNull CaptureRequest request,
+                                                   @NonNull TotalCaptureResult result) {
+                        // super.onCaptureCompleted(session, request, result);// no-op
+                        if (picturesTaken.lastEntry() != null) {
+                            // OPTIMIZE: design error: service should notify listeners to handler
+                            // image data, not save those data to SD-card then notify listeners.
+                            capturingListener.onCaptureDone(picturesTaken.lastEntry().getKey(), picturesTaken.lastEntry().getValue());
+                            Log.i(TAG, "done taking picture from camera " + cameraDevice.getId());
+                        }
+                        closeCamera();
+                    }
+                };
+            }
+
+            private void closeCamera() {
+                Log.d(TAG, "closing camera " + cameraDevice.getId());
+                cameraDevice.close();
+                reader.close();
+            }
+
+            @Override
+            public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+            }
+        };
     }
-
-
-    private void saveImageToDisk(final byte[] bytes) {
-        final String cameraId = this.cameraDevice == null ? UUID.randomUUID().toString() : this.cameraDevice.getId();
-        final File file = new File(Environment.getExternalStorageDirectory() + "/" + cameraId + "_pic.jpg");
-        try (final OutputStream output = new FileOutputStream(file)) {
-            output.write(bytes);
-            this.picturesTaken.put(file.getPath(), bytes);
-        } catch (final IOException e) {
-            Log.e(TAG, "Exception occurred while saving picture to external storage ", e);
-        }
-    }
-
-    private void takeAnotherPicture() {
-        this.currentCameraId = this.cameraIds.poll();
-        openCamera();
-    }
-
-    private void closeCamera() {
-        Log.d(TAG, "closing camera " + cameraDevice.getId());
-        if (null != cameraDevice && !cameraClosed) {
-            cameraDevice.close();
-            cameraDevice = null;
-        }
-        if (null != imageReader) {
-            imageReader.close();
-            imageReader = null;
-        }
-    }
-
 
 }
